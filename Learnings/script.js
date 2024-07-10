ac23f77b5efee233eaae537f709ce29b6ed26464
Iran Credit Scoring <ics.xai.team@gmail.com>
2024-07-10 09:48:19 +0000

    6th

--- Learnings/script.js
+++ Learnings/script.js
@@ -2,14 +2,14 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     function updateAge() {
-      const birthday = new Date('2024-07-01T00:00:00Z'); // Your birthday in ISO format
+      const birthday = new Date('2018-01-10T00:00:00Z'); // Your birthday in ISO format
       const now = new Date();
       const ageInMilliseconds = now - birthday;
       const ageInSeconds = ageInMilliseconds / 1000;
       const ageInYears = ageInSeconds / (60*60*24*365.25)
       const ageInDays = ageInSeconds / (60*60*24)
   
-      document.getElementById('Days').innerText = ageInDays.toFixed(6); // Display age in Years
+      document.getElementById('Years').innerText = ageInYears.toFixed(7); // Display age in Years
       // document.getElementById('Seconds').innerText = ageInSeconds.toFixed(2); // Display age in seconds with three decimals for milliseconds
       
     }

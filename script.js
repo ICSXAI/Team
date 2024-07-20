5416caa0e773e52b2ae8226fa5521ceee2c407f4
Iran Credit Scoring <ics.xai.team@gmail.com>
2024-07-20 05:09:20 +0000

    #

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
       const now = new Date();
       const ageInMilliseconds = now - birthday;
       const ageInSeconds = ageInMilliseconds / 1000;
-      const ageInYears = ageInSeconds / (60*60*24*365.25)
+      const ageInMinutes = ageInSeconds / (60)
+      const ageInHours = ageInSeconds / (60*60)
       const ageInDays = ageInSeconds / (60*60*24)
-      const ageInHours = (ageInSeconds / (60*60)) - (Math.floor(ageInDays) * 24)
-      const ageInSec = ageInSeconds - (Math.floor(ageInDays)*24*3600)
+      const ageInYears = ageInSeconds / (60*60*24*365.25)
+
+      const ageInHour = (ageInSeconds / (60*60)) - (Math.floor(ageInDays) * 24)
+      const ageInMin = (ageInSeconds / (60)) - (Math.floor(ageInHours) * 60)
+      const ageInSec = (ageInSeconds / (1)) - (Math.floor(ageInMinutes) * 60)
   
       document.getElementById('Days').innerText = ageInDays.toFixed(0);
-      document.getElementById('Hours').innerText = ageInHours.toFixed(0);
+      document.getElementById('Hours').innerText = ageInHour.toFixed(0);
+      document.getElementById('Minutes').innerText = ageInMin.toFixed(0);
       document.getElementById('Seconds').innerText = ageInSec.toFixed(0);
     
       // Display age in Years
